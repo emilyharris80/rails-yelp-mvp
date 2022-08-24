45e00d801eaa40594e28d3d81b8b7a8daf695b5b
emily  harris <emily.harris@simplybusiness.co.uk>
2022-08-24 15:17:09 +0100

    rails yelp mvp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = %w(chinese italian japanese french belgian)
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian), message: "%{value} is not a valid cuisine" }
+  validates :category, presence: true, inclusion: { in: CATEGORY, message: "%{value} is not a valid cuisine" }
   has_many :reviews, dependent: :destroy
 end
